da7ae925834cf9c83b253009cbfa152c16211c35
Evis98 <evaduranescobar@gmail.com>
2020-09-09 19:12:09 -0600

    Pantallas

--- src/presentacion/empresa/Empresa_View.java
+++ src/presentacion/empresa/Empresa_View.java
@@ -22,7 +22,7 @@ public class Empresa_View extends javax.swing.JInternalFrame implements Observer
     public Empresa_View() {
         initComponents();
         this.setLocation(100, 100);
-        this.getContentPane().setBackground(new java.awt.Color(204, 255, 204));
+
     }
 
     /**
